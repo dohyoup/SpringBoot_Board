c97132af30bcec05516f325adef21920d89b86c2
dohyoup <dohyoup@naver.com>
2023-11-29 21:54:31 +0900

    게시판 목록 조회

--- src/main/java/com/codingrecipe/board/controller/BoardController.java
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -2,9 +2,13 @@ package com.codingrecipe.board.controller;
 
 import com.codingrecipe.board.dto.BoardDTO;
 import com.codingrecipe.board.service.BoardSevice;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,8 +24,15 @@ public class BoardController {
 
     @PostMapping("/save")
     public String save(@ModelAttribute BoardDTO boardDTO) {
-        System.out.println("boardDTO = " + boardDTO);
         boardSevice.save(boardDTO);
         return "index";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        //DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다.
+        List<BoardDTO> boardDTOList = boardSevice.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
     }
 }
--- src/main/java/com/codingrecipe/board/service/BoardSevice.java
+++ src/main/java/com/codingrecipe/board/service/BoardSevice.java
@@ -6,6 +6,9 @@ import com.codingrecipe.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // DTO -> Entity (Entity Class)
 // Entity -> DTO (DTO Class)
 
@@ -17,5 +20,14 @@ public class BoardSevice {
     public void save(BoardDTO boardDTO) {
         BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
         boardRepository.save(boardEntity);
+    }
+
+    public List<BoardDTO> findAll() {
+        List<BoardEntity> boardEntityList = boardRepository.findAll();
+        List<BoardDTO> boardDTOList = new ArrayList<>();
+        for(BoardEntity boardEntity : boardEntityList) {
+            boardDTOList.add(BoardDTO.toBoardDTO(boardEntity));
+        }
+        return boardDTOList;
     }
 }
