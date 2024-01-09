dfd16df692cd8d94eb8137f09b88e8d90971618b
dohyoup <dohyoup@naver.com>
2024-01-09 22:57:38 +0900

    12_ajax댓글처리_작성 내용 서버 요청

---
+++ src/main/java/com/codingrecipe/board/controller/CommentController.java
@@ -0,0 +1,22 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.CommentDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/comment")
+public class CommentController {
+
+    @PostMapping("/save")
+    @ResponseBody
+    public String save(@ModelAttribute CommentDTO commentDTO) {
+        System.out.println("commentDTO = " + commentDTO);
+        return "요청 성공";
+    }
+}
